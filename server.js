5d801aad7b6d9a18eec13f0c30d4582e10565f20
Vinodrs1979 <vrsapplications@gmail.com>
2026-08-31 09:25:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // 1. MONGODB DATABASE CONNECTION
 // ==========================================
 // ध्यान दें: नीचे अपना असली MongoDB लिंक डालें (पासवर्ड के साथ)
-const MONGO_URI = "यहाँ_अपना_MONGODB_LINK_डालें";
+const MONGO_URI = "mongodb+srv://vrsapplications_db_user:Tc5xuE80F79m3xha@cluster0.abcde.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log("✅ MongoDB Connected Successfully!"))
